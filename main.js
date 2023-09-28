274c6416ab9e76da3f2a6eec6d9f3397926a9cc1
tar.gergo <tar.gergo21@gmail.com>
2023-09-28 21:26:02 +0200

    alapok meg csinálása

---
+++ main.js
@@ -0,0 +1,7 @@
+class onelet{
+constructor(){
+    const Header = $("header");
+    const ARTICLE = $("article");
+    HEADER.append('<dic class="myImage"><img src="kepek/..."></div>')
+}
+}
---
+++ theme.js
@@ -0,0 +1,4 @@
+function sotet() {
+    document.body.classList.toggle("dark-theme");
+    document.body.classList.toggle("light-theme");
+    } 
