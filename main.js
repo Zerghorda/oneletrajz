39951febbd0364e6c9861b864ba07280037d3af7
Tar Gergő <tar.gergo21@gmail.com>
2023-10-06 11:01:59 +0200

    hmtl,css kész,javascript kezdés

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 $(function () {
   const ARTICLE = $("article");
+  const SIDEMANU = $("icons");
   function sotet() {
     document.body.classList.toggle("dark-theme");
     document.body.classList.toggle("light-theme");
+    function iconsalahuz() {
+
+    }
+
   }
 });
